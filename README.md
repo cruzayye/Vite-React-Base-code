0145d1a870273bf4307403dffbee361df2f17870
cruzayye <johnnyc010392@gmail.com>
2025-05-12 16:25:55 -0700

    new base code setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Website starter Todos
+- Run index.html file through AI to align with new website
+- update package.json with new website info
+- set up test branch in netlify
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
-import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SocialIcons from "./socialIcons/SocialIcons";
 
 
-
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
+
+
+  return (
+    <BrowserRouter>
+      <main className="App">
+        <h1>Get started on your site</h1>
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          {plateRoutes()}
+          <Route path="/locations" element={<Locations />} />
+        </Routes> */}
+        {/* <SocialIcons /> */}
+      </main>
+    </BrowserRouter>
+  );
 };
 
 const container = document.getElementById("root");
-const root = createRoot(container)
-
-root.render(React.createElement(App));
-
-// https://react-v8.holt.courses/lessons/no-frills-react/components
+const root = createRoot(container);
+root.render(<App />);
